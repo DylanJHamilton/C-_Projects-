f5aadc991e0f8da3561fd83fe0619b8bc7b49120
Dylan Hamilton <dhamcle43@gmail.com>
2019-06-20 10:51:43 -0400

    Update on Calculator, Organization

---
+++ APIs and Apps/Basic Calculator/Calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+//Very Basic Calculator
+int main()
+{
+
+//Variable Declarations
+int num1, num2;
+char operation;
+int result; 
+
+//Communication between program and user. 
+std::cout << "Enter your first number: ";
+std::cin >> num1; //Assigns value for Num1
+
+std::cout << "Enter your second number: ";
+std::cin >> num2;  //Assigns value for Num2
+
+//Checks the condition based on user input
+if(operation == '+'){
+    result = num1 + num2;
+}
+ else if(operation == '-'){
+    result = num1 - num2;
+ }
+ else if(operation == '*'){
+    result = num1 * num2;
+ }
+ else if(operation == '/'){
+    result = num1 / num2;
+ } 
+  else{
+    std::cout << "Invalid Operation: Try again."; 
+    }
+
+//Returns the result.
+std::cout << result;
+
+return 0;
+
+}
---
+++ APIs and Apps/Basic Calculator/README.md
@@ -0,0 +1,5 @@
+<h1>Basic Calculator</h1>
+
+Console Application that does basic calculation operations between Addition, Subtraction, Multiplication, Division and Modulus between <b>two numbers.</b>
+
+Mostly a basic calculator through the terminal.
